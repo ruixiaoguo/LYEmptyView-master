886781171f094394e03c802a81699e88ccb07185
ruixiaoguo <grx0917@sina.com>
2018-10-24 13:33:53 +0800

    Initial commit

---
+++ LYEmptyViewDemo/Classes/Demo/DemoEmptyView.m
@@ -0,0 +1,43 @@
+//
+//  DemoEmptyView.m
+//  LYEmptyViewDemo
+//
+//  Created by liyang on 2017/12/1.
+//  Copyright © 2017年 liyang. All rights reserved.
+//
+
+#import "DemoEmptyView.h"
+
+@implementation DemoEmptyView
+
++ (instancetype)diyEmptyView{
+    
+    return [DemoEmptyView emptyViewWithImageStr:@"noData"
+                                       titleStr:@"暂无数据"
+                                      detailStr:@"请稍后再试!"];
+}
+
++ (instancetype)diyEmptyActionViewWithTarget:(id)target action:(SEL)action{
+
+    return [DemoEmptyView emptyActionViewWithImageStr:@"noNetwork"
+                                             titleStr:@"无网络连接"
+                                            detailStr:@"请检查你的网络连接是否正确!"
+                                          btnTitleStr:@"重新加载"
+                                               target:target
+                                               action:action];
+}
+
+- (void)prepare{
+    [super prepare];
+    
+//    self.backgroundColor = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1];
+    
+    self.autoShowEmptyView = NO;
+    
+    self.titleLabTextColor = MainColor(180, 30, 50);
+    self.titleLabFont = [UIFont systemFontOfSize:18];
+    
+    self.detailLabTextColor = MainColor(80, 80, 80);
+}
+
+@end
